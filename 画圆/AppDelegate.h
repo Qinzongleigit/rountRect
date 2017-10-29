18eb1d994fa1e260c450e49a1a8def10f1389c50
覃宗雷 <qinzonglei@tanzongleideiMac.local>
2017-10-29 16:03:53 +0800

    Initial Commit

---
+++ 画圆/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  画圆
+//
+//  Created by 覃宗雷 on 2017/10/29.
+//  Copyright © 2017年 leizongqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 画圆/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  画圆
+//
+//  Created by 覃宗雷 on 2017/10/29.
+//  Copyright © 2017年 leizongqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
